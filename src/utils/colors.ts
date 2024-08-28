f5f913501040ab65f4616119c0e98eb5c11be9d7
Yanga <yangagovuza@gmail.com>
2024-08-28 10:31:13 +0200

    backend server 2

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -25,5 +25,5 @@ export const colors = {
 export const apiEndPoint = process.env.REST_ENDPOINT || `https://legend-receipt-b.vercel.app`
 
 //export const apiEndPoint = process.env.REST_ENDPOINT || `http://localhost:4200`
-//export const  apiEndPoint = process.env.REST_ENDPOINT || `https://legend-receipt-f.vercel.app`
+//export const  apiEndPoint = process.env.REST_ENDPOINT || `https://legend-receipt-b.vercel.app`
 //https://legend-receipt-b.vercel.app
